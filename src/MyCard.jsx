25e2e3664318f2d488b714500be5de25909d8f4c
Koza4ok <khrystsina.kozak@gmail.com>
2024-03-01 19:50:19 +0100

    done 3 levels. added animation to Next and Previous

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -1,6 +1,7 @@
 import { Indicator } from "./Indicator";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
 export function MyCard({
   handleNext,
@@ -10,82 +11,153 @@ export function MyCard({
   handleDotClick,
 }) {
   const currentCardData = tutorialData[index];
+  const [leftPosition, setLeftPosition] = useState("0px");
+  const [isVisible, setIsVisible] = useState(true);
+
+  const slideAndNext = () => {
+    setLeftPosition("-1000px");
+
+    setTimeout(() => {
+      setIsVisible(false);
+    }, "400");
+
+    setTimeout(() => {
+      setIsVisible(true);
+      setLeftPosition("1000px");
+    }, "450");
+
+    setTimeout(() => {
+      setIsVisible(true);
+      setLeftPosition("0px");
+      handleNext();
+    }, "800");
+  };
+
+  const slideAndLeft = () => {
+    setLeftPosition("1000px");
+
+    setTimeout(() => {
+      setIsVisible(false);
+    }, "400");
+
+    setTimeout(() => {
+      setIsVisible(true);
+      setLeftPosition("-1000px");
+    }, "450");
+
+    setTimeout(() => {
+      setIsVisible(true);
+      setLeftPosition("0px");
+      handlePrevious();
+    }, "800");
+  };
+
+  const slideDot = (i) => {
+    setLeftPosition("-1000px");
+
+    setTimeout(() => {
+      setIsVisible(false);
+    }, "400");
+
+    setTimeout(() => {
+      setIsVisible(true);
+      setLeftPosition("1000px");
+    }, "450");
+
+    setTimeout(() => {
+      setIsVisible(true);
+      setLeftPosition("0px");
+      handleDotClick(i);
+    }, "800");
+  };
 
   return (
     <div className="main">
-      <Card style={{ width: "18rem", margin: "auto" }}>
-        <Card.Img
-          variant="top"
-          src={currentCardData.image}
+      {isVisible && (
+        <Card
           style={{
-            backgroundColor: currentCardData.bgColor,
-            width: "100%",
-            height: "170px",
-            paddingTop: "80px",
-            paddingBottom: "80px",
-            borderTopLeftRadius: "20px",
-            borderTopRightRadius: "20px",
-          }}
-        />
-        <Card.Body
-          style={{
-            backgroundColor: "#FFFFFF",
-            padding: "45px",
-            borderBottomLeftRadius: "20px",
-            borderBottomRightRadius: "20px",
+            width: "18rem",
+            margin: "auto",
+            position: "relative",
+            transition: "all 0.3s",
+            left: leftPosition,
           }}
         >
-          <Card.Title style={{ fontWeight: "bold" }}>
-            {currentCardData.title}
-          </Card.Title>
-          <Card.Text style={{ opacity: 0.8, fontSize: "14px", height: "50px" }}>
-            {currentCardData.description}
-          </Card.Text>
-
-          {index !== tutorialData.length - 1 && (
-            <Button
-              variant="primary"
-              style={{
-                fontSize: "15px",
-                backgroundColor: "#141414",
-                width: "30px",
-                height: "30px",
-                borderRadius: "65px",
-                marginRight: "5px",
-                color: "white", 
-                marginTop: "10px",
-                float: "right",
-              }}
-              onClick={handleNext}
-            >
-              &#11106;
-            </Button>
-          )}
-          {index > 0 && (
-            <Button
-              variant="primary"
-              style={{
-                fontSize: "15px",
-                width: "30px",
-                height: "30px",
-                borderRadius: "65px",
-                marginRight: "5px",
-                marginTop: "10px",
-                float: "right",
-              }}
-              onClick={handlePrevious}
-            >
-              &#11104;
-            </Button>
-          )}
-
-          <Indicator
-            tutorialData={tutorialData}
-            step={index}
-            handleDotClick={handleDotClick}
+          <Card.Img
+            variant="top"
+            src={currentCardData.image}
+            style={{
+              backgroundColor: currentCardData.bgColor,
+              width: "100%",
+              height: "170px",
+              paddingTop: "80px",
+              paddingBottom: "80px",
+              borderTopLeftRadius: "20px",
+              borderTopRightRadius: "20px",
+            }}
           />
-        </Card.Body>
-      </Card>
+          <Card.Body
+            style={{
+              backgroundColor: "#FFFFFF",
+              padding: "45px",
+              borderBottomLeftRadius: "20px",
+              borderBottomRightRadius: "20px",
+            }}
+          >
+            <Card.Title style={{ fontWeight: "bold" }}>
+              {currentCardData.title}
+            </Card.Title>
+            <Card.Text
+              style={{ opacity: 0.8, fontSize: "14px", height: "50px" }}
+            >
+              {currentCardData.description}
+            </Card.Text>
+
+            {index !== tutorialData.length - 1 && (
+              <Button
+                variant="primary"
+                style={{
+                  fontSize: "15px",
+                  backgroundColor: "#141414",
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "65px",
+                  marginRight: "5px",
+                  color: "white",
+                  marginTop: "10px",
+                  float: "right",
+                }}
+                onClick={slideAndNext}
+              >
+                &#11106;
+              </Button>
+            )}
+            {index > 0 && (
+              <Button
+                variant="primary"
+                style={{
+                  fontSize: "15px",
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "65px",
+                  marginRight: "5px",
+                  marginTop: "10px",
+                  float: "right",
+                }}
+                onClick={slideAndLeft}
+              >
+                &#11104;
+              </Button>
+            )}
+
+            <Indicator
+              tutorialData={tutorialData}
+              step={index}
+              handleDotClick={slideDot}
+            />
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 }
